e73865418c7da6db4284aa791d5f7d0b92c4d6ae
emartin <e.martinc.2019@alumnos.urjc.es>
2020-02-19 18:16:06 +0100

    not finished

--- P0/Ex2.py
+++ P0/Ex2.py
@@ -1,4 +1,17 @@
-from
+from Seq0 import *
 
-def seq_read_fasta("U5.txt"):
+FOLDER = "../session-04/"
+FILENAME = "U5.txt"
+
+DNA_FILE = FOLDER + FILENAME
+
+# -- Open the DNA file
+seq = seq_read_fasta(DNA_FILE)
+
+print("------> Exercise 2")
+print(f"DNA file: {FILENAME}")
+
+print("The first 20 bases are:")
+print(seq[:20])
+
 
--- P0/Seq0.py
+++ P0/Seq0.py
@@ -1,5 +1,22 @@
+from pathlib import Path
+
 def seq_ping():
     print("OK!")
-
 seq_ping()
 
+def seq_read_fasta(filename):
+
+    """
+    Read a file with a DNA sequence in FASTA format
+    :param filename: String
+    :return: String
+    """
+
+    # -- Read the file
+    contents = Path(filename).read_text()
+
+    # -- Remove the head
+    body = contents.split('\n')[1:]
+
+    # -- Return the body as a string
+    return "".join(body)
